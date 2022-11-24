3ddaa41b8035ed80c939b747c2ca4f774220144e
Datengeist <janikfo04@gmail.com>
2022-11-24 11:20:51 +0100

    delete the useless lines

--- DayOne.py
+++ DayOne.py
@@ -1,5 +1,4 @@
 j: int =0
-output = open("./output.txt", "w")
 content: list[str]
 
 with open("./input.txt", "r", encoding="UTF-16") as f:
@@ -9,7 +8,6 @@ for line in content:
     j+=1
     verarbeitung: int= 2020 - int(line)
     ausgabe: int= int(line) * verarbeitung
-    output.write(str(ausgabe) + "\n")
     for i in range(0,j):
         if (int(content[i])+int(line)) == 2020:
             print(str(i) + " " + str(j) + " " + str(int(content[i])*int(line)))
